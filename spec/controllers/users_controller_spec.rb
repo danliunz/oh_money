b31c31e93d6a2e1667057fa74e7777a2dcba08df
Dan Liu <danliu@Dans-MacBook-Pro-2.local>
2016-03-08 11:00:11 +1300

    compromise a little for testing remember-me

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -1,84 +1,82 @@
 require 'rails_helper'
 
-# TODO: write feature test case to simulate the case
-# 1. user signs in with 'remember-me' enabled
-# 2. then user signs in again with 'remember-me' disabled
 RSpec.describe UsersController, type: :controller do
   describe "GET #signup" do
     it "assigns a new user record" do
       get :signup_form
       expect(assigns[:user]).to be_a_new(User)
     end
-    
+
     it "returns http success" do
       get :signup_form
       expect(response).to have_http_status(:success)
     end
   end
-  
+
   describe "POST #sigin" do
     fixtures :users
     let(:user) { users(:danliu) }
-    
+
     context "when user authentication succeeds" do
       let(:valid_crendential) do
         { user: { name: user.name, password: "danliu" } }
       end
-    
+
       before(:each) do
         post :signin, valid_crendential
       end
-      
+
       it "assigns @user with record from DB" do
         expect(assigns[:user]).not_to be_new_record
         expect(assigns[:user].name).to eq(user.name)
       end
-      
+
       it "creates a user session" do
         expect(session[:current_user_id]).to eq(user.id)
       end
-      
+
       it "redirects to root url" do
         expect(response).to redirect_to(root_url)
       end
-      
+
       context "when remember-me is enabled" do
         let(:valid_crendential) do
           {
-            user: { name: user.name, password: "danliu" }, 
-            remember_me: "any_value" 
+            user: { name: user.name, password: "danliu" },
+            remember_me: "any_value"
           }
         end
-        
-        it "stores user id and remember token into cookies" do
+
+        it "stores user id, token and created time into cookies" do
           expect(cookies.signed[:remember_me_user_id]).to eq(user.id)
           expect(cookies[:remember_me_token]).not_to be_empty
+          expect(cookies.signed[:remember_me_created_at]).not_to be_nil
         end
       end
     end
-    
+
     context "when user authentication fails" do
       def expect_render_signin_form_with_alert
          expect(response).to render_template(:signin_form)
          expect(flash.now[:alert]).to match(/invalid/i)
       end
-      
+
       context "with non-existing username" do
         let(:invalid_credential) do
           { user: { name: "oblivion", password: "do not matter" } }
         end
-        
+
         it "renders signin form with alert" do
           post :signin, invalid_credential
           expect_render_signin_form_with_alert
         end
       end
-      
+
       context "with wrong password" do
         let(:invalid_credential) do
           { user: { name: user.name, password: "wrong!" } }
         end
-       
+
         it "renders signin form with alert" do
           post :signin, invalid_credential
           expect_render_signin_form_with_alert
@@ -86,28 +84,28 @@ RSpec.describe UsersController, type: :controller do
       end
     end
   end
-  
+
   describe "#signout" do
     fixtures :users
     let(:user) { users(:danliu) }
     let(:valid_crendential) do
       { user: { name: user.name, password: "danliu" } }
     end
-      
-    before(:each) do 
+
+    before(:each) do
       post :signin, valid_crendential
       post :signout
     end
-    
+
     it "removes current user id from session" do
       expect(session[:current_user_id]).to be_nil
     end
-    
+
     it "removes remember-me data from cookie" do
       expect(cookies[:remember_me_user_id]).to be_nil
       expect(cookies[:remember_me_token]).to be_nil
     end
-    
+
     it "removes remember-me token from DB" do
       expect(Account::RememberMe.find_by(user: user)).to be_nil
     end
--- spec/controllers/welcome_controller_spec.rb
+++ spec/controllers/welcome_controller_spec.rb
@@ -1,22 +1,34 @@
 require 'rails_helper'
 
+# Rationale: considering session/cookie access is impossible
+# in non-rack based test in Cucumber/Capybara, we choose to
+# verify remember-me in some controller test which we can manipulate
+# cookie easily
 RSpec.describe WelcomeController, type: :controller do
   fixtures :users
   let(:user) { users(:danliu) }
-  
-  # TODO: move the test case to better place
+
   describe "GET #index" do
-    it "sets current user by remember-me" do
+    render_views
+
+    def simulate_remember_me_present
       token = Security::RandomToken.new
       user.create_remember_me!(digest_token: token.digest)
-      
+
       cookies.signed[:remember_me_user_id] = user.id
       cookies[:remember_me_token] = token.to_s
       cookies.signed[:remember_me_created_at] = Time.now.to_i
-      
+    end
+
+    it "discovers current user by remember-me" do
+      simulate_remember_me_present
+      expect(session[:current_user_id]).to be_nil
+
       get :index
-      
+
+
       expect(assigns[:current_user].id).to eq(user.id)
+      expect(controller.user_signed_in?).to be true
     end
   end
 end
