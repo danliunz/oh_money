2991724702a2f9430775c58f6bcf1b430a1f6f1e
Dan Liu <danliu@Dans-MacBook-Pro-2.local>
2016-03-15 15:34:11 +1300

    enhance  user experience for adding tag to expense entry

--- app/assets/javascripts/expense_entries.js
+++ app/assets/javascripts/expense_entries.js
@@ -1,17 +1,38 @@
 $(document).ready(function() {
-  init_add_expense_entry_tag();
+  init_add_tag();
 });
 
-function init_add_expense_entry_tag() {
-  $('#add_expense_entry_tag_btn').click(function(event) {
-    tag = $('#expense_entry_tag_input').val().trim();
-    if (tag) {
-      $('.tags-panel').append(
-        '<span class="label label-success">' + tag + '</span>'
-      ).append(
-        '<input type="hidden" name="expense_entry[tags][][name]"' +
-        '       value="' + tag + '" />'
-      );
+function add_tag() {
+  tag = $('#tag_input').val().trim();
+  if (tag) {
+    $('.tags-panel').append(
+      '<span class="label label-success">' + tag + '</span>'
+    ).append(
+      '<input type="hidden" name="expense_entry[tags][][name]"' +
+      '       value="' + tag + '" />'
+    );
+
+    // clear input for next tag entry
+    $('#tag_input').val("");
+  }
+}
+
+function init_add_tag() {
+  // add a new tag
+  $('#add_tag_btn').click(function(event) {
+    add_tag();
+
+    $('#tag_input').focus();
+  });
+
+  // press enter in text input
+  // 1. add a new tag
+  // 2. DO NOT submit the form
+  $('#tag_input').keypress(function(event) {
+    if (event.which == 13) { // enter key
+      add_tag();
+      event.preventDefault();
     }
+
   });
 }
