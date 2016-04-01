f61cc36951a326848bfc909fdf205b369cece644
Dan Liu <danliu@Dans-MacBook-Pro-2.local>
2016-04-01 13:37:27 +1300

    Temporary workaround to run application in production mode

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -20,7 +20,9 @@ Rails.application.configure do
   # config.action_dispatch.rack_cache = true
 
   # Disable Rails's static asset server (Apache or nginx will already do this).
-  config.serve_static_assets = false
+  # TODO: revert and use nginx to serve static files
+  # TODO: avoid copying bootstrap fonts from venter/assets/fonts to public/assets
+  config.serve_static_assets = true
 
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
