2c966e6357e230a097f538122a91faf29dd41059
Dan Liu <danliu@Dans-MacBook-Pro-2.local>
2016-04-06 16:22:23 +1200

    show total cost of each day in 'expense history' view

--- app/helpers/expense_entries_helper.rb
+++ app/helpers/expense_entries_helper.rb
@@ -6,10 +6,14 @@ module ExpenseEntriesHelper
     expense_entries.each do |expense_entry|
       if current_date != expense_entry.purchase_date
         current_date = expense_entry.purchase_date
-        expense_entries_by_date << { date: current_date, entries: [] }
+
+        expense_entries_by_date << {
+          date: current_date, entries: [], total_cost: 0
+        }
       end
 
       expense_entries_by_date.last[:entries] << expense_entry
+      expense_entries_by_date.last[:total_cost] += expense_entry.cost
     end
 
     expense_entries_by_date
