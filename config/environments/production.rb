68c672b9d35dca80929bb5d26b62a9c3dbd0ac9f
Dan Liu <danliu@Dans-MacBook-Pro-2.local>
2016-04-22 10:41:31 +1200

    remove redundant config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -21,8 +21,8 @@ Rails.application.configure do
 
   # Disable Rails's static asset server (Apache or nginx will already do this).
   # TODO: avoid copying bootstrap fonts from vendor/assets/fonts to public/assets
-  # TODO: avoid copying jstree images and fonts to public/assets
-  config.serve_static_assets = true
+  #       avoid copying jstree images and fonts to public/assets
+  config.serve_static_assets = false
 
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
